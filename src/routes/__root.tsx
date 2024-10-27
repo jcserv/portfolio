2354fe99d0480bb388d8d32be8f56008215f37c3
jcserv <jarrod.servilla@gmail.com>
2024-10-27 01:15:22 -0400

    tweak: remove tanstack dev router

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Analytics } from "@vercel/analytics/react"
 
 export const Route = createRootRoute({
@@ -11,7 +10,6 @@ export const Route = createRootRoute({
       <Outlet />
       <Footer />
       <Analytics />
-      <TanStackRouterDevtools />
     </>
   ),
 });

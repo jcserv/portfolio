f432f7b34b408dbc5f445e65784583c4e3e82d83
jcserv <jarrod.servilla@gmail.com>
2024-10-27 01:15:00 -0400

    chore: add analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Analytics } from "@vercel/analytics/react"
 
 export const Route = createRootRoute({
   component: () => (
@@ -9,6 +10,7 @@ export const Route = createRootRoute({
       <Header />
       <Outlet />
       <Footer />
+      <Analytics />
       <TanStackRouterDevtools />
     </>
   ),

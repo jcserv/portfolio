402788742b77a57e315ac83de609f87f670f3694
jcserv <jarrod.servilla@gmail.com>
2024-11-10 00:38:21 -0500

    tweak: remove vercel insights/analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { ActiveExpProvider } from "@/context/ActiveExpProvider";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Index } from "./routes";
 import "./index.css";
 
@@ -14,8 +12,6 @@ export default function App() {
         <Header />
         <Index />
         <Footer />
-        <Analytics />
-        <SpeedInsights />
       </ActiveExpProvider>
     </ThemeProvider>
   );

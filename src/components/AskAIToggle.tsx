861b6b8be229c45c757bd3a2770911eaa3367dc3
jcserv <jarrod.servilla@gmail.com>
2024-11-17 18:43:19 -0500

    tweak: hide ai toggle when ai_enabled is false

--- src/components/AskAIToggle.tsx
+++ src/components/AskAIToggle.tsx
@@ -8,29 +8,36 @@ import {
   TooltipTrigger,
 } from "./ui/tooltip";
 
-export const AskAIToggle = ({ className }: { className?: string }) => (
-  <div className="flex items-center space-x-1.5">
-    <Label htmlFor="ask-ai" className="text-xs whitespace-nowrap">
-      Ask AI ✨
-    </Label>
-    <TooltipProvider>
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger>
-          <Switch
-            id="ask-ai"
-            disabled
-            className={`scale-75 mt-1 ${className}`}
-            aria-label="This feature is coming soon"
-            aria-disabled="true"
-            onMouseEnter={() =>
-              captureEvent(analyticsEvents.HOVER, {
-                target: "ask-ai-toggle",
-              })
-            }
-          />
-        </TooltipTrigger>
-        <TooltipContent side="bottom">Coming soon!</TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  </div>
-);
+const IS_AI_ENABLED = import.meta.env.VITE_AI_ENABLED == "true";
+
+export const AskAIToggle = ({ className }: { className?: string }) => {
+  if (!IS_AI_ENABLED) {
+    return null;
+  }
+  return (
+    <div className="flex items-center space-x-1.5">
+      <Label htmlFor="ask-ai" className="text-xs whitespace-nowrap">
+        Ask AI ✨
+      </Label>
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger>
+            <Switch
+              id="ask-ai"
+              disabled
+              className={`scale-75 mt-1 ${className}`}
+              aria-label="This feature is coming soon"
+              aria-disabled="true"
+              onMouseEnter={() =>
+                captureEvent(analyticsEvents.HOVER, {
+                  target: "ask-ai-toggle",
+                })
+              }
+            />
+          </TooltipTrigger>
+          <TooltipContent side="bottom">Coming soon!</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+};
